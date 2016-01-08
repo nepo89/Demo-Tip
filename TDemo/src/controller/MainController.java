90267795e47f79cff07f339b38a9edaade44e694
nepo89 <dpklementova@gmail.com>
2016-01-09 00:00:13 +0100

    Cambiando el nombre de la funcion function_down to function_up en la clase MainController

--- TDemo/src/controller/MainController.java
+++ TDemo/src/controller/MainController.java
@@ -10,9 +10,9 @@ public class MainController {
 		System.out.print("Ahoj svìto!");
 		System.out.print("\n");
 		//hello
-		function_down();
+		function_up();
 	}
-	private static void function_down(){
+	private static void function_up(){
 		
 	} 
 
