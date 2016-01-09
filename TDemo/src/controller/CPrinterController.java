40bb54623f858ee05498cd6429b0045844e91142
nepo89 <dpklementova@gmail.com>
2016-01-09 10:29:21 +0100

    Update CPrinterController.java

--- TDemo/src/controller/CPrinterController.java
+++ TDemo/src/controller/CPrinterController.java
@@ -1,5 +1,19 @@
 package controller;
 
 public class CPrinterController {
+  	private int id;
 
+	/**
+	 * @return the id
+	 */
+	public int obtenerId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void establecerId(int id) {
+		this.id = id;
+	}
 }
