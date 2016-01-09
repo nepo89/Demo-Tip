ff960e1d8a552afcf976a8fcf8472cb500a3b185
nepo89 <dpklementova@gmail.com>
2016-01-09 10:27:55 +0100

    Estableciendo la variable miembro y sus metodos get y set

--- TDemo/src/controller/CPrinterController.java
+++ TDemo/src/controller/CPrinterController.java
@@ -1,5 +1,19 @@
 package controller;
 
 public class CPrinterController {
+	private int id;
 
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
 }
